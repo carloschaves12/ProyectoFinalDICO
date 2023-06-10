ca4d9985e790f68c93a1249723c9a936246e68fa
carloschaves12 <carloschaveshernandez2@gmail.com>
2023-06-10 18:32:48 +0200

    slider finished and images changed

--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Slider
-  const imgslider1 = document.getElementById("slider1");
-  const imgslider2 = document.getElementById("slider2");
 
+  const arrayImages = [
+    document.getElementById("slider1"),
+    document.getElementById("slider2"),
+    document.getElementById("slider3"),
+    document.getElementById("slider4"),
+  ];
+
+  let currentImage = 0;
+  let nextImage = 1;
   setInterval(changeImages, 5000);
 
   function changeImages() {
-    console.log("changeImages");
-    if (imgslider1.classList.contains("active")) {
-      console.log("slider1 active");
-      imgslider1.classList.remove("active");
-      imgslider2.classList.add("active");
-    } else {
-      console.log("slider2 active");
-      imgslider2.classList.remove("active");
-      imgslider1.classList.add("active");
+
+    if (nextImage >= arrayImages.length) {
+      arrayImages[currentImage].classList.remove("active");
+      currentImage = 0;
+      nextImage = 1;
+      arrayImages[currentImage].classList.add("active");
+      return;
     }
+    arrayImages[currentImage].classList.remove("active");
+    arrayImages[nextImage].classList.add("active");
+    currentImage = nextImage;
+    nextImage++;
   }
 
   // Al pasar el boton sobre la imagen, se muestra el boton de "ver mas"
+  setInterval(botonLlevar, 1000);
   const botonImagen = document.getElementById("boton-imagen");
   const imagen = botonImagen.previousElementSibling;
 
-  imagen.addEventListener("mouseover", () => {
-    botonImagen.style.opacity = 1;
-  });
-
-  imagen.addEventListener("mouseout", () => {
+  function botonLlevar() {
     botonImagen.style.opacity = 0;
-  });
+    
+    if (window.innerWidth > 1024) {
+      imagen.addEventListener("mouseover", () => {
+        botonImagen.style.opacity = 1;
+      });
+
+      imagen.addEventListener("mouseout", () => {
+        botonImagen.style.opacity = 0;
+      });
+    }
+  }
 
   // Funcionalidad al icono del menú
   const menuIcon = document.getElementById("menu-icon");
